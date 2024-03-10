c19300e18a994f83936530a5dd6ee5598634a6a0
barkas218 <julianhoegel@web.de>
2024-03-10 01:19:21 +0100

    d

---
+++ src/pattern/factory/FactoryPattern.java
@@ -0,0 +1,17 @@
+package pattern.factory;
+
+public class FactoryPattern {
+    public static void start(){
+
+        /*
+            Definition
+                The factory pattern defnes an interface for creating an bject but lets subclasses decide which class to instantiate.
+
+                Factory methods let a class defer instantiation to subclasses
+
+            Simple Factory Patern
+
+         */
+
+    }
+}
